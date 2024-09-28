3a8a187d4978f771ad8d8d94504ab2e725bb9649
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-09-28 19:47:00 +0200

    fix not visible knight metal shields

--- src/main/java/twilightforest/client/model/TFLayerDefinitions.java
+++ src/main/java/twilightforest/client/model/TFLayerDefinitions.java
@@ -179,7 +179,5 @@ public class TFLayerDefinitions {
 		EntityModelLayerRegistry.registerModelLayer(TFModelLayers.MOONWORM, MoonwormModel::create);
 
 		EntityModelLayerRegistry.registerModelLayer(TFModelLayers.RED_THREAD, RedThreadModel::create);
-
-		EntityModelLayerRegistry.registerModelLayer(TFModelLayers.KNIGHTMETAL_SHIELD, KnightmetalShieldModel::create);
 	}
 }
