d4df91933820b8802e32ec7fe665c2b1ccc2fb0c
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-10-04 15:01:34 +0200

    delete new model classes

--- src/main/java/twilightforest/client/renderer/entity/newmodels/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package twilightforest.client.renderer.entity.newmodels;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
