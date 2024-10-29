2a9afc10c124fee8828ea834837c8736a9c2e10f
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-10-29 07:08:59 +0100

    add TFRegistryAccess

--- src/main/java/twilightforest/init/custom/TFDatapackBuiltinEntriesProvider.java
+++ src/main/java/twilightforest/init/custom/TFDatapackBuiltinEntriesProvider.java
@@ -74,31 +74,7 @@ public class TFDatapackBuiltinEntriesProvider extends RegistriesDatapackGenerato
     var builderKeys = new HashSet<>(datapackEntriesBuilder.entries.stream().map(RegistrySetBuilder.RegistryStub::key).toList());
     getDataPackRegistriesWithDimensions().filter(data -> !builderKeys.contains(data.key())).forEach(data -> datapackEntriesBuilder.add(data.key(), context -> {}));
 
-    var registryAccess = new RegistryAccess.Frozen() {
-      Registry<? extends Registry<?>> registryOfRegistries = BuiltInRegistries.REGISTRY;
-
-      public <T> Optional<Registry<T>> registry(ResourceKey<? extends Registry<? extends T>> registryKey) {
-        Stream<RegistryEntry<?>> registries = registries();
-        Optional<RegistryEntry<?>> entry = registries.filter(obj -> obj.key().equals(registryKey)).findFirst();
-        return Optional.of((Registry<T>) entry.get().value());
-      }
-
-      public Stream<RegistryEntry<?>> registries() {
-        var oldStream = registryOfRegistries.entrySet().stream().map(RegistryEntry::fromMapEntry);
-        Stream newStream = oldStream.filter(obj -> {
-          var value = obj.value();
-          if (value.key().location().getNamespace().equals(TwilightForestMod.REGISTRY_NAMESPACE)) {
-            return false;
-          }
-          return true;
-        });
-        return newStream;
-      }
-
-      public Frozen freeze() {
-        return this;
-      }
-    };
+    var registryAccess = new TFRegistryAccess();
     return datapackEntriesBuilder.buildPatch(registryAccess, original);
   }
 
