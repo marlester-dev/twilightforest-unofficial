693c7bb94227427d2c3498d24eceb9365b34a684
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-12-09 18:47:52 +0100

    use a simple for loop instead of foreach

--- src/main/java/twilightforest/compat/wthit/QuestRamWoolProvider.java
+++ src/main/java/twilightforest/compat/wthit/QuestRamWoolProvider.java
@@ -3,12 +3,14 @@ package twilightforest.compat.wthit;
 import com.google.common.collect.Iterables;
 import mcp.mobius.waila.api.*;
 import mcp.mobius.waila.api.component.ItemListComponent;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import twilightforest.entity.passive.QuestRam;
 import twilightforest.util.ColorUtil;
 
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Map;
 
 public enum QuestRamWoolProvider implements IEntityComponentProvider {
 
@@ -18,14 +20,13 @@ public enum QuestRamWoolProvider implements IEntityComponentProvider {
 	public void appendBody(ITooltip tooltip, IEntityAccessor entityAccessor, IPluginConfig config) {
 		if (entityAccessor.getEntity() instanceof QuestRam ram) {
 			ArrayList<ArrayList<ItemStack>> woolArrayListsArrayList = new ArrayList<>();
-			AtomicInteger woolCount = new AtomicInteger(0);
-			ColorUtil.WOOL_TO_DYE_IN_RAM_ORDER.forEach((color, block) -> {
-				if (!ram.isColorPresent(color)) {
-					int originalWoolCount = woolCount.getAndIncrement();
-					ItemStack woolItemStack = new ItemStack(block);
+			int woolCount = 0;
+			for (Map.Entry<DyeColor, Block> entry : ColorUtil.WOOL_TO_DYE_IN_RAM_ORDER.entrySet()) {
+				if (!ram.isColorPresent(entry.getKey())) {
+					ItemStack woolItemStack = new ItemStack(entry.getValue());
 
 					ArrayList<ItemStack> lastWoolList;
-					if (woolArrayListsArrayList.isEmpty() || originalWoolCount % 8 == 0) {
+					if (woolArrayListsArrayList.isEmpty() || woolCount % 8 == 0) {
 						lastWoolList = new ArrayList<>();
 						lastWoolList.add(woolItemStack);
 						woolArrayListsArrayList.add(lastWoolList);
@@ -33,8 +34,9 @@ public enum QuestRamWoolProvider implements IEntityComponentProvider {
 						lastWoolList = Iterables.getLast(woolArrayListsArrayList);
 						lastWoolList.add(woolItemStack);
 					}
+					woolCount++;
 				}
-			});
+			}
 			for (ArrayList<ItemStack> woolArrayList : woolArrayListsArrayList) {
 				tooltip.addLine(new ItemListComponent(woolArrayList, 1,  0.5F));
 			}
